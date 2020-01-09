c7f9bd3ef8566ed1be46f0ad3166c33c34aeb933
Luana Faria Lane <luanasfff@gmail.com>
2020-01-09 05:53:09 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -10,7 +10,14 @@ class Person
     #person.partner = self
   #end
   
-  
+   def get_married(person)
+    self.partner = person
+    if person.class != Person
+      raise PartnerError
+    else
+      person.partner = self
+    end
+  end
   
  class PartnerError < StandardError
   end
